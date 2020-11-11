bfc56376134c92709600a1941c45d611ac105cab
pumelotea <535553297@qq.com>
2020-11-11 16:50:15 +0800

    make framework

---
+++ src/components/DemoCom.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>DemoCom</div>
+</template>
+
+<script>
+export default {
+  name: "DemoCom"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     Home
+    <demo-com></demo-com>
   </div>
 </template>
 
 <script>
+import DemoCom from "@/components/DemoCom";
 export default {
-  name: "Home"
+  name: "Home",
+  components:{
+    DemoCom
+  }
 }
 </script>
 
